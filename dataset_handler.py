564bf1c6e673610b02c50722674dea5667072d90
denper19 <jermito24@gmail.com>
2024-11-17 01:03:34 -0800

    added dataset handler

---
+++ dataset_handler.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class DatasetHandler:
+
+    def __init__(self, filename):
+        """
+        Reads the data into a dataframe and does initial processing
+        """
+        assert isinstance(filename, str)
+        assert len(filename) > 0
+
+        try:
+            self._df = pd.read_csv(filename)
+        except FileNotFoundError:
+            print("The file could not be found!")
+
+    def clean_dataset(self):
+        """
+        Cleans the dataset i.e removes NaNs, errors, etc... and makes the dataset simpler to use.
+        """
+        # if we run the following, we see that Legislative District has the most number of NaN values
+        self._df.isna().sum().to_frame('NaN')
+        # we can fix this as follows:
+        self._df['Legislative District'] = self._df['Legislative District'].map(lambda x: str(x).split('.')[0])
+        # in a similar way for Vehicle Location
+        # forward fill propigates the last observed values to fill the NaN slots
+        self._df['Vehicle Location'] = self._df['Vehicle Location'].ffill()
+        # Secondly, Legislative District is of dtype object, when it should be float so we can change it
+
+        # We can rename the electric vehicle types for simplicity, i.e Plug-in Hybrid Electric Vehicle (PHEV) to PHEV
+
+    def create_boxplot(self, data={}):
+        """_summary_
+
+        :param data: _description_, defaults to {}
+        :type data: dict, optional
+        """
+        fig = plt.figure()
+        ax  = fig.add_subplot(111)
+        plt.boxplot(data["data"])
+        plt.xlabel(data["xlabel"])
+        plt.ylabel(data["ylabel"])
+        # ax.set_xticklabels(years)
+        plt.title(data['title'])
+        plt.tight_layout()
+
+    def create_histogram(self, data={}):
+        """_summary_
+
+        :param data: _description_, defaults to {}
+        :type data: dict, optional
+        """
+        pass
+
+    def create_piechart(self, data={}):
+        """_summary_
+
+        :param data: _description_, defaults to {}
+        :type data: dict, optional
+        """
+        pass
+
+    def create_map(self, data={}):
+        """_summary_
+
+        :param data: _description_, defaults to {}
+        :type data: dict, optional
+        """
+        pass
